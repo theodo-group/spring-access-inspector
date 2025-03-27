509fe8df29694591486b37086199ce8c000ec1eb
victor <victor.collodel@theodo.com>
2025-03-27 11:05:06 +0100

    feat(html generator): extract in a proper class

--- inspector/src/main/java/com/theodo/inspector/SpringAccessInspector.java
+++ inspector/src/main/java/com/theodo/inspector/SpringAccessInspector.java
@@ -1,9 +1,7 @@
 package com.theodo.inspector;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,6 +16,7 @@ import com.theodo.inspector.cli.InspectorCommand;
 import com.theodo.inspector.impl.PreAuthorizeAnnotationProcessing;
 import com.theodo.inspector.impl.ast.ASTReader;
 import com.theodo.inspector.impl.utils.AnnotationDto;
+import com.theodo.inspector.impl.utils.HtmlTableGenerator;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -59,37 +58,8 @@ public class SpringAccessInspector extends InspectorCommand implements Annotatio
     @Override
     public Integer call() throws Exception {
         List<AnnotationDto> annotations = analyzer();
-        generateHtmlTable(annotations);
+        HtmlTableGenerator.generateHtmlTable(annotations, this.htmlOutputFile);
         return 0;
-    }
-
-    public void generateHtmlTable(List<AnnotationDto> annotations) {
-
-        // Generate the HTML table
-        StringBuilder htmlTable = new StringBuilder();
-        htmlTable.append("\n<head>\n<style>\n")
-                .append("table {\nborder-collapse: collapse;\nwidth:100%;\n}\n")
-                .append("th, td {\npadding: 8px;\ntext-align: left;\nborder-bottom: 1px solid #ddd;\n}\n")
-                .append("th {\nbackground-color: #f2f2f2;\n}\n")
-                .append("</style>\n</head>\n<body>\n")
-                .append("<table>\n<tr {}>\n<th>Endpoint</th>\n<th>Method</th>\n<th>PreAuthorize</th>\n</tr>\n");
-
-        // Iterate over the list and generate each row of the table
-        for (AnnotationDto annotation : annotations) {
-            htmlTable.append("<tr>\n<td>").append(annotation.endpoint()).append("</td>\n")
-                    .append("<td>").append(annotation.method().replace("Mapping", "")).append("</td>\n")
-                    .append("<td>").append(annotation.preAuthorize()).append("</td>\n</tr>\n");
-        }
-
-        // Close the HTML table and body
-        htmlTable.append("</table>\n</body>\n</html>");
-
-        // Write the HTML table to a file
-        try (PrintWriter writer = new PrintWriter(this.htmlOutputFile)) {
-            writer.println(htmlTable);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     public static Stream<File> findPoms(String basePath) throws IOException {
