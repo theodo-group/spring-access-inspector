ed9f89a99287af6ff38e71df620aaa588b904f60
victor <victor.collodel@theodo.com>
2025-04-28 17:24:05 +0200

    config(install): generate java string of js file on build

---
+++ inspector/scripts/generate-embedded-assets.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+# ==========================
+# Script description
+# ==========================
+
+# This script generates a Java class containing the content of a JavaScript file as a string constant.
+# It is used to embed the JavaScript file into our HTML output.
+# The script is run as part of the build process to ensure that the JavaScript file is always up to date, thanks to exec-maven-plugin.
+
+# ==========================
+# Configuration
+# ==========================
+
+JS_FILE="src/main/resources/sortTable.js"
+JAVA_FILE="target/generated-sources/embedded/com/theodo/inspector/impl/utils/EmbeddedAssets.java"
+PACKAGE_NAME="com.theodo.inspector.impl.utils"
+CONSTANT_NAME="SORT_TABLE_JS"
+
+# ==========================
+# Script
+# ==========================
+
+if [ ! -f "$JS_FILE" ]; then
+    echo "Error : The $JS_FILE file does not exist."
+    exit 1
+fi
+
+mkdir -p "$(dirname "$JAVA_FILE")"
+
+{
+echo "package $PACKAGE_NAME;"
+echo ""
+echo "public class EmbeddedAssets {"
+echo ""
+echo "    public static final String $CONSTANT_NAME = \"\"\""
+} > "$JAVA_FILE"
+
+cat "$JS_FILE" >> "$JAVA_FILE"
+
+{
+echo "\"\"\";"
+echo "}"
+} >> "$JAVA_FILE"
+
+echo "✅ EmbeddedAssets.java generated in $JAVA_FILE"
