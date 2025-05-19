29967013ee8adc4dbb6bf9b7fe60aa3ddeff31f1
victor <victor.collodel@theodo.com>
2025-05-19 11:43:41 +0200

    feat(generated-embedded-assets): now handle multiple js files

--- inspector/scripts/generate-embedded-assets.sh
+++ inspector/scripts/generate-embedded-assets.sh
@@ -6,25 +6,24 @@ set -e
 # Script description
 # ==========================
 
-# This script generates a Java class containing the content of a JavaScript file as a string constant.
-# It is used to embed the JavaScript file into our HTML output.
-# The script is run as part of the build process to ensure that the JavaScript file is always up to date, thanks to exec-maven-plugin.
+# This script generates a Java class containing the content of all JavaScript files in the src/main/resources folder as string constants.
+# It is used to embed the JavaScript files into our HTML output.
+# The script is run as part of the build process to ensure that the JavaScript files are always up to date, thanks to exec-maven-plugin.
 
 # ==========================
 # Configuration
 # ==========================
 
-JS_FILE="src/main/resources/sortTable.js"
+JS_FOLDER="src/main/resources"
 JAVA_FILE="target/generated-sources/embedded/com/theodo/inspector/utils/EmbeddedAssets.java"
 PACKAGE_NAME="com.theodo.inspector.utils"
-CONSTANT_NAME="SORT_TABLE_JS"
 
 # ==========================
 # Script
 # ==========================
 
-if [ ! -f "$JS_FILE" ]; then
-    echo "Error : The $JS_FILE file does not exist."
+if [ ! -d "$JS_FOLDER" ]; then
+    echo "Error: The $JS_FOLDER folder does not exist."
     exit 1
 fi
 
@@ -35,13 +34,23 @@ echo "package $PACKAGE_NAME;"
 echo ""
 echo "public class EmbeddedAssets {"
 echo ""
-echo "    public static final String $CONSTANT_NAME = \"\"\""
 } > "$JAVA_FILE"
 
-cat "$JS_FILE" >> "$JAVA_FILE"
+# Iterate over all JavaScript files in the folder
+for js_file in "$JS_FOLDER"/*.js; do
+    if [ -f "$js_file" ]; then
+        # Convert file name to uppercase with underscores between words
+        constant_name=$(basename "$js_file" .js | sed -E 's/([a-z])([A-Z])/\1_\2/g' | tr '[:lower:]' '[:upper:]' | tr '-' '_' | tr '.' '_')_JS
+        {
+        echo "    public static final String $constant_name = \"\"\""
+        cat "$js_file"
+        echo "\"\"\";"
+        echo ""
+        } >> "$JAVA_FILE"
+    fi
+done
 
 {
-echo "\"\"\";"
 echo "}"
 } >> "$JAVA_FILE"
 
